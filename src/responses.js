08b72d2768b5434b2dc6362d2e25bdb171d352cf
Justin Morris <jgm1844@g.rit.edu>
2020-09-26 22:08:36 -0400

    plus index

--- src/responses.js
+++ src/responses.js
@@ -1,76 +1,74 @@
-const http = require('http');
+// const http = require('http');
 const url = require('url');
 
 const MIME = {
-	json: "application/json",
-	css: "text/css",
-	html: "text/html",
+  json: 'application/json',
+  css: 'text/css',
+  html: 'text/html',
 };
 
 const users = {};
 
-const respond = (request, response, content, contentType = "application/json", statusCode = 200) =>
-{
-	response.writeHead(statusCode, { "Content-Type": contentType });
-	response.write(content);
-	response.end();
-};
-
-//#region Get 404
-const get404 = (request, response) => respond(request, response, JSON.stringify({id:404,message:"Not Found"}), "application/json", 404);
-const head404 = (request, response) => respond(request, response, JSON.stringify({id:404}), "application/json", 404);
-//#endregion
-
-//#region Get Users
-const getUsers = (request, response) => respond(request, response, JSON.stringify(users), "application/json", 200);
-const headUsers = (request, response) => respond(request, response, JSON.stringify({"id":200,"message":"Success"}), "application/json", 200);
-//#endregion
-
-const postUser = (req, res) =>
-{
-	let query = parseURL(req);
-	let message, code;
-	if (query.name && query.age) {
-		if (users[query.name]) {
-			code = 204;
-			message = JSON.stringify({"id":204,"message":"No Content"});
-		}
-		else {
-			code = 201;
-			message = JSON.stringify({"id":201,"message":"Created"});
-		}
-		users[query.name] = query.age;
-	}
-	else {
-		code = 400;
-		message = JSON.stringify({"id":400,"message":"Both name and age are required."});
-	}
-	respond(req, res, message, MIME.json, code);
-}
-
-//#region Get Styles
-const fs = require('fs');
-const styles = fs.readFileSync(`${__dirname}/../client/style.css`);
-const getStyles = (request, response) => respond(request, response, styles, "text/css");
-//#endregion
-//#region Get HTML
-const page = fs.readFileSync(`${__dirname}/../client/client.html`);
-const getHTML = (request, response) => respond(request, response, page, "text/html");
-//#endregion
-
 const parseURL = (request) => url.parse(request.url, true).query;
 
+const respond = (request, response, content, contentType = 'application/json', statusCode = 200) => {
+  response.writeHead(statusCode, { 'Content-Type': contentType });
+  response.write(content);
+  response.end();
+};
+
+// #region Get 404
+const get404 = (request, response) => respond(request, response, JSON.stringify({ id: 404, message: 'Not Found' }), 'application/json', 404);
+const head404 = (request, response) => respond(request, response, JSON.stringify({ id: 404 }), 'application/json', 404);
+// #endregion
+
+// #region Get Users
+const getUsers = (request, response) => respond(request, response, JSON.stringify(users), 'application/json', 200);
+const headUsers = (request, response) => respond(request, response, JSON.stringify({ id: 200, message: 'Success' }), 'application/json', 200);
+// #endregion
+
+const postUser = (req, res) => {
+  const query = parseURL(req);
+  let message; let
+    code;
+  if (query.name && query.age) {
+    if (users[query.name]) {
+      code = 204;
+      message = JSON.stringify({ id: 204, message: 'No Content' });
+    } else {
+      code = 201;
+      message = JSON.stringify({ id: 201, message: 'Created' });
+    }
+    users[query.name] = query.age;
+  } else {
+    code = 400;
+    message = JSON.stringify({ id: 400, message: 'Both name and age are required.' });
+  }
+  respond(req, res, message, MIME.json, code);
+};
+
+// #region Get Styles
+const fs = require('fs');
+
+const styles = fs.readFileSync(`${__dirname}/../client/style.css`);
+const getStyles = (request, response) => respond(request, response, styles, 'text/css');
+// #endregion
+// #region Get HTML
+const page = fs.readFileSync(`${__dirname}/../client/client.html`);
+const getHTML = (request, response) => respond(request, response, page, 'text/html');
+// #endregion
+
 module.exports = {
-	getHTML,
-	getStyles,
-	get404,
-	head404,
+  getHTML,
+  getStyles,
+  get404,
+  head404,
 
-	getUsers,
-	headUsers,
-	postUser,
+  getUsers,
+  headUsers,
+  postUser,
 
-	parseURL,
+  parseURL,
 };
 // respond(request, response, `<!DOCTYPE html>
 // <html lang="en">
@@ -78,6 +76,6 @@ module.exports = {
 //   <title>Error</title>
 // </head>
 // <body>
-  
+
 // </body>
 // </html>`, "text/html");
--- src/server.js
+++ src/server.js
@@ -4,72 +4,61 @@ const responses = require('./responses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-const onRequest = (request, response) =>
-{
-	let jumpTable = {
-		// "/": (args) => responses.getHTML(request, response),
-		"/": responses.getHTML,
-		"/index": responses.getHTML,
-		"/style.css": responses.getStyles,
-		"/getUsers": () => {
-			switch (request.method) {
-				case 'HEAD':
-					responses.headUsers(request, response);
-					break;
-				case 'GET':
-				default:
-					responses.getUsers(request, response);
-					break;
-			}
-		},
-		"/notReal": () => {
-			switch (request.method) {
-				case 'HEAD':
-					responses.head404(request, response);
-					break;
-				case 'GET':
-				default:
-					responses.get404(request, response);
-					break;
-			}
-		},
-		"/addUser": () => {
-			switch (request.method) {
-				case 'POST':
-					responses.postUser(request, response);
-					break;
-				case 'GET':
-				default:
-					responses.get404(request, response);
-					break;
-			}
-		},
-		"404": responses.get404,
-	};
-	console.log(request.url);
-	let plainURL;
-	// if ((request.url + "").indexOf("?") > 0)
-	// 	plainURL = (request.url + "").substring(0, (request.url + "").indexOf("?"));
-	// else
-	// 	plainURL = (request.url + "");
-	// console.log(request.host);
-	// console.log(request.path);
-	// console.log(request);
-	let parsedURL = new url.URL(request.url, `http://${request.headers.host}`);//"http://127.0.0.1:3000/"
-	// console.log(parsedURL);
-	plainURL = parsedURL.pathname;
-	console.log(plainURL);
-	// if (typeof(jumpTable[request.url]) != "function") {
-	if (typeof(jumpTable[plainURL]) != "function") {
-		jumpTable[404](request, response);
-	}
-	else {
-		// jumpTable[request.url](request, response);
-		jumpTable[plainURL](request, response);
-	}
-}
+const onRequest = (request, response) => {
+  const jumpTable = {
+    // "/": (args) => responses.getHTML(request, response),
+    '/': responses.getHTML,
+    '/index': responses.getHTML,
+    '/style.css': responses.getStyles,
+    '/getUsers': () => {
+      switch (request.method) {
+        case 'HEAD':
+          responses.headUsers(request, response);
+          break;
+        case 'GET':
+        default:
+          responses.getUsers(request, response);
+          break;
+      }
+    },
+    '/notReal': () => {
+      switch (request.method) {
+        case 'HEAD':
+          responses.head404(request, response);
+          break;
+        case 'GET':
+        default:
+          responses.get404(request, response);
+          break;
+      }
+    },
+    '/addUser': () => {
+      switch (request.method) {
+        case 'POST':
+          responses.postUser(request, response);
+          break;
+        case 'GET':
+        default:
+          responses.get404(request, response);
+          break;
+      }
+    },
+    404: responses.get404,
+  };
+  console.log(request.url);
+  const parsedURL = new url.URL(request.url, `http://${request.headers.host}`);// "http://127.0.0.1:3000/"
+  const plainURL = parsedURL.pathname;
+  console.log(plainURL);
+  // if (typeof(jumpTable[request.url]) != "function") {
+  if (typeof (jumpTable[plainURL]) !== 'function') {
+    jumpTable[404](request, response);
+  } else {
+    // jumpTable[request.url](request, response);
+    jumpTable[plainURL](request, response);
+  }
+};
 
-console.log("Server starting up...");
+console.log('Server starting up...');
 
 http.createServer(onRequest).listen(port);
 console.log(`Listening on 127.0.0.1:${port}`);
